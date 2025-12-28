b24b35bd1b52cf264228218304584b91b52cd42b
Harley <tkgs.zhang.hailing@gmail.com>
2025-12-28 11:04:34 +0800

    ininital commit to set up question project
    add git ignore and moved folders

---
+++ PlantMonitorUnitTest/AlarmTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using InterviewEmptyProject.Services;
+
+namespace InterviewEmptyUnitTest;
+
+public class Tests
+{
+    private Alarm _Target;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _Target = new Alarm();
+    }
+
+    [Test]
+    public void GivenBelowRangeAlarmOn()
+    {
+        
+    }
+}
