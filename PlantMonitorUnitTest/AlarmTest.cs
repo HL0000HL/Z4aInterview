777e0df8b31cc392bb1bb61a3a9178f02fe3c885
Harley <tkgs.zhang.hailing@gmail.com>
2025-12-28 12:07:50 +0800

    refactor test

--- PlantMonitorUnitTest/AlarmTest.cs
+++ PlantMonitorUnitTest/AlarmTest.cs
@@ -15,15 +15,43 @@ public class Tests
         _Target = new Alarm(_sensorWrapper);
     }
 
-    [Test]
-    public void GivenBelowRangeWhenGetMeasurementThenAlarmOn()
+    [TestCase(16)]
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(long.MinValue)]
+    public void GivenBelowRangeWhenGetMeasurementThenAlarmStart(long measurement)
+    {
+        GivenCurrentAlarmIsOff();
+        WhenSensorMeasurementIs(measurement);
+        ThenAlarmStart();
+    }
+    
+    [TestCase(22)]
+    [TestCase(long.MaxValue)]
+    public void GivenAboveRangeWhenGetMeasurementThenAlarmStart(long measurement)
+    {
+        GivenCurrentAlarmIsOff();
+        WhenSensorMeasurementIs(measurement);
+        ThenAlarmStart();
+    }
+    
+
+    private void ThenAlarmStart()
+    {
+        _Target.AlarmOn.Should().BeTrue();
+        _Target.AlarmCount.Should().Be(1);
+    }
+
+    private void WhenSensorMeasurementIs(long measurement)
+    {
+        _sensorWrapper.SetMeasurementForTest(measurement);
+        _Target.Check();
+    }
+
+    private void GivenCurrentAlarmIsOff()
     {
         _Target.AlarmOn.Should().BeFalse();
         _Target.AlarmCount.Should().Be(0);
-        _sensorWrapper.SetMeasurementForTest(14);
-        _Target.Check();
-        _Target.AlarmOn.Should().BeTrue();
-        _Target.AlarmCount.Should().Be(1);
     }
 }
 
